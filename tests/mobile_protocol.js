958bc384e036e12f508c0767ee489a35ebd48543
fabioberger <fabioberger1991@gmail.com>
2013-06-17 21:04:34 -0700

    removed remote folder

---
+++ tests/mobile_protocol.js
@@ -0,0 +1,4 @@
+var mongoose = require('mongoose').connect('mongodb://localhost/auto_test');
+
+// uncomment to pre-populate with test company and agent
+// require('./config/populate').populateDB();
